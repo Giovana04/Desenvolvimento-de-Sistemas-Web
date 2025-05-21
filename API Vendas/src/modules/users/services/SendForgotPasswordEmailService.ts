c37ae4f1aa08eae0d9cb9352fa90bd8bb58127e4
Giovana04 <giovanafn0401@gmail.com>
2025-05-21 10:15:49 -0300

    21-05

--- API Vendas/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ API Vendas/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,27 +1,37 @@
 import { getCustomRepository } from "typeorm";
 import UserRepository from "../typeorm/repositories/UsersRepository";
-import UserTokensRepository from "../typeorm/repositories/UserTokensRepository";
+import UserTokensRepository from "@modules/users/typeorm/repositories/UserTokensRepository";
 import AppError from "src/shared/errors/AppError";
 import EtherealMail from "@config/mail/EtherealMail";
+import path from 'path'
+import { link } from "joi";
 
 interface IRequest{
     email: string;
 }
 
-export default class SendForgotPasswordEmailServise{
+export default class SendForgotPasswordEmailService{
     public async execute({email} : IRequest) : Promise<void>{
         const userRepository = getCustomRepository(UserRepository);
         const userTokenRepository = getCustomRepository(UserTokensRepository);
+        const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
         const user = await userRepository.findByEmail(email);
         if(!user){
             throw new AppError('User does not exist.');
         }
         const {token} = await userTokenRepository.createToken(user.id);
         //implementação do envio do token
-        console.log(token);
+        console.log(user.id);
         await EtherealMail.sendMail({
-            to: email,
-            body: `Solicitação de recuperação de senha recebida: ${token}`,
+            to: {name: user.name, email:user.email},
+            subject: '[API VENDAS] Recuperação de Senha',
+            templateData: {
+                file: forgotPasswordTemplate,
+                variables: {
+                    name: user.name,
+                    link: `http://localhost:3000/reset_password?token=${token}`
+                }
+            }
         });
     }
 }
